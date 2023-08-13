38a27928ed522b68f7298ea7bdbc3e8693072a31
Gabrielshow <adefelashogbanmu@gmail.com>
2023-08-14 00:46:34 +0100

    fixing errors

--- cochack_tutorial_python_17_OOP.py
+++ cochack_tutorial_python_17_OOP.py
@@ -31,36 +31,36 @@ print(res)
 
 #class methods
 #they are marked with the classmethod decorator
-# class Rectangle():
-#     def _init_(self, width, height):
-#         self.width = width
-#         self.height = height
-#     
-#     def calculate_area(self):
-#         return self.width * self.width
-#     
-#     @classmethod
-#     def new_square(cls, side_length):
-#         return cls(side_length, side_length)
-#     
-# square = Rectangle.new_square(5)
-# print(square.calculate_area())
+class Rectangle():
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    
+    def calculate_area(self):
+        return self.width * self.width
+    
+    @classmethod
+    def new_square(cls, side_length):
+        return cls(side_length, side_length)
+    
+square = Rectangle.new_square(5)
+print(square.calculate_area())
 
 #self and cls are just conventions
 
-# class Person():
-#     def _init_(self, name):
-#         self.name = name
-#         
-#     @classmethod
-#     def sayHi(cls):
-#         print("Hi ")
+class Person():
+    def __init__(self, name):
+        self.name = name
+        
+    @classmethod
+    def sayHi(cls):
+        print("Hi ")
 
 #static methods are similar to class methods except they don't receive any additional arguments
 #they are marked with static method decorator
 
-class Pizza(toppings):
-    def _init_(self, toppings):
+class Pizza:
+    def __init__(self, toppings):
         self.toppings = toppings
         
     @staticmethod
@@ -72,44 +72,44 @@ class Pizza(toppings):
         
 toppings = ["cheese" , "onions", "spam"]
 if all(Pizza.validate_toppings(i) for i in toppings):
-    pizza = Pizza(ingredients)
+    pizza = Pizza(toppings)
 #need to fix code outputs Pizza() takes no argument
 
 # property
 # created by putting a property decorator above a method
 
-# class Pizza:
-#     def _init_(self, toppings):
-#         self.toppings = toppings
-#         
-#     @property
-#     def pineapple_allowed(self):
-#         return False
-#     
-# Pizza = Pizza(["cheese", "tomato"])
-# print(Pizza.pineapple_allowed)
+class Pizza1:
+    def __init__(self, toppings):
+        self.toppings = toppings
+        
+    @property
+    def pineapple_allowed(self):
+        return False
+    
+Pizza = Pizza1(["cheese", "tomato"])
+print(Pizza.pineapple_allowed)
 # Pizza.pineapple_allowed = True
 
 # getters and setteres
-# class Pizza:
-#     def _init_(self, toppings):
-#         self.toppings = toppings
-#         self._pineapple_allowed = False
-#     
-#     @property
-#     def pineapple_allowed(self):
-#         return self._pineapple_allowed
-#     
-#     @pineapple_allowed.setter
-#     def pineapple_allowed(self, value):
-#         if value:
-#             password = input("Enter the password")
-#             if password == 'swordfish':
-#                 self._pineapple_allowed = value
-#             else:
-#                 raise ValueError("Alert!, Intruder!")
-#     
-# pizza = Pizza(["cheese", "tomato"])
-# print(pizza.pineapple_allowed)
-# pizza.pineapple_allowed = True
-# print(pizza.pineapple_allowed)
+class Pizza2:
+    def __init__(self, toppings):
+        self.toppings = toppings
+        self._pineapple_allowed = False
+    
+    @property
+    def pineapple_allowed(self):
+        return self._pineapple_allowed
+    
+    @pineapple_allowed.setter
+    def pineapple_allowed(self, value):
+        if value:
+            password = input("Enter the password ")
+            if password == 'swordfish':
+                self._pineapple_allowed = value
+            else:
+                raise ValueError("Alert!, Intruder!")
+    
+pizza = Pizza2(["cheese", "tomato"])
+print(pizza.pineapple_allowed)
+pizza.pineapple_allowed = True
+print(pizza.pineapple_allowed)
